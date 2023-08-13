67d0798d9c1fa416fc3f64ecd4e2501dceba11cc
InnoSufiyan <innosufiyan@gmail.com>
2023-08-13 10:11:28 -0700

    string concatenation

---
+++ JavaScript/Basics/code/class2.js
@@ -0,0 +1,64 @@
+// var firstName = "Muhammad"
+// var lastName = 'Sufiyan'
+
+// var fullName = firstName + " " + lastName
+
+// var ajeebSiCalculation = "alif" - "baay"
+
+// var mobileNumberCode = "0342"
+// var mobileNumber = "2318760"
+
+// var number1 = "222"
+// var number2 = 342
+
+// var input = prompt("What is your name")
+// var inputAge = prompt("What is your age")
+
+
+
+
+// var carDescription = "people call my car as sufiyan's car"
+
+// var carDescription = `people call my car as sufiyan's car`
+
+// var number = 32;
+// console.log(number, "==>>line number 25")
+
+// number = number + 5
+// number += 5
+// console.log(number, "==>>line number 28")
+
+// var a = 5;            //5
+
+// a++                   //5
+//                       //6
+// console.log(a++)      //6                  
+// console.log(a--)      //7
+// // console.log(--a)      //5
+
+// var a = 5;            //5
+//             //5  +  5  +  6  -  7  +   6 + 6
+// var incDec = a++ + --a + ++a - ++a + --a + a--
+
+// //21
+// //18
+// //3
+
+var score1 = 50;
+var score2 = 20;
+var score3 = 30;
+
+var totalScore = score1 + score2 + score3
+var averageScore = totalScore / 3
+
+console.log(totalScore, averageScore)
+
+var plates = 20;
+var people = 7;
+
+var remainingPlates = plates % people
+console.log(remainingPlates)
+
+remainingPlates += 1;
+
+console.log("The remaining plates are " + remainingPlates)
