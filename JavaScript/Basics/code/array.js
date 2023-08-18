557d55e31cf284ee59d509cbea1dfc62837799fc
InnoSufiyan <innosufiyan@gmail.com>
2023-08-18 10:17:11 -0700

    array and functions

--- JavaScript/Basics/code/array.js
+++ JavaScript/Basics/code/array.js
@@ -1,22 +1,72 @@
-// var number = prompt("Dil main pehley number per kon hai")
+// // var number = prompt("Dil main pehley number per kon hai")
 
-// var arr = ["girl friend", "Beghum"]
+// // var arr = ["girl friend", "Beghum"]
 
-// alert(arr[number])
+// // alert(arr[number])
 
 
-// const student1 = "saqib"
-// const student2 = "fahad"
-// const student3 = "zain"
-// const student4 = "abbas"
+// // const student1 = "saqib"
+// // const student2 = "fahad"
+// // const student3 = "zain"
+// // const student4 = "abbas"
 
-// const students = ["saqib", "fahad", "zain", "abbas"] //arrays
-//                       //0           //1        //2          //3
-// const bakeryShelf = ["pasteries", "paapay", "plain cake", "chips", "biscuits", "bun", "pepsi", 25, null, undefined, "chaaclate"]
+// // const students = ["saqib", "fahad", "zain", "abbas"] //arrays
+// //                       //0           //1        //2          //3
+// // const bakeryShelf = ["pasteries", "paapay", "plain cake", "chips", "biscuits", "bun", "pepsi", 25, null, undefined, "chaaclate"]
 
-// let whichItemNumber = prompt("Which item do you need, kindly tell the number")
+// // let whichItemNumber = prompt("Which item do you need, kindly tell the number")
 
-// alert("Here you are with your product " + bakeryShelf[whichItemNumber])
-// alert("Here you are with your product " + bakeryShelf[bakeryShelf.length - 7])
+// // alert("Here you are with your product " + bakeryShelf[whichItemNumber])
+// // alert("Here you are with your product " + bakeryShelf[bakeryShelf.length - 7])
 
-// alert("Seth sahab, aap k peson sey, total " + bakeryShelf.length + " products rakhey hain bakery main")
+// // alert("Seth sahab, aap k peson sey, total " + bakeryShelf.length + " products rakhey hain bakery main")
+
+// const menu = ["daal chawal", "bhindi", "white karahi", "torai", "tikka biryani"]
+
+// // console.log(menu)
+
+// // menu[2] = "aalo palak"
+
+// // console.log(menu)
+
+// // const name = "sufiyan"
+
+// // name = "farhan"
+
+// let name = "sufiyan"
+
+// let name2 = name    //sufiyan
+
+// console.log(name, name2)
+// name = "farhan"
+
+// console.log(name, name2)
+//                          //a           //b          //c           //d          //e
+// let foodMenuOffice1 = ["daal chawal", "bhindi", "white karahi", "torai", "tikka biryani"]
+//                     //    a  b  c   d  e
+// let foodMenuOffice2 = foodMenuOffice1
+
+// console.log(foodMenuOffice1)
+// console.log(foodMenuOffice2)
+//                        //c
+// foodMenuOffice2[2] = "aalo palak"
+
+// console.log(foodMenuOffice1)
+// console.log(foodMenuOffice2)
+
+var fruits = ["aam", "orangi", "banana", "grapes", "apples", "strawberry"]
+
+// console.log(fruits.length, "===>>length")
+           //6
+// fruits[fruits.length + 5] = "kuch bhi"
+
+// fruits[fruits.length] = 47
+
+// // console.log(fruits, "==>>my fruits")
+
+// var myFruit = fruits[0]
+
+// fruits[fruits.length - 1] = "amrood"
+
+// console.log(myFruit)
+// console.log(fruits)
--- JavaScript/Basics/code/functions.js
+++ JavaScript/Basics/code/functions.js
@@ -20,21 +20,107 @@
 // betaPaniBharDo()
 
 
-function betaBharKLao(liquidType) {
-    console.log("Yeh lo beta glass")
-    console.log("sambhal k pakarna isko, kiyun k sheeshay ka hai")
-    console.log("samney jao , " + liquidType + " cooler k pass")
-    console.log("ek hogi tooti, isko kholo")
-    console.log(liquidType + " ko glass main girney do")
-    console.log("dekhna glass sey baahir "+ liquidType +" na nikley")
-    console.log("jesay hi glass bharney lagey, to tooti band kardo")
-    console.log("wapis meray pass ana, sambhal k " + liquidType +" k glass ko le kar aana")
+// function betaBharKLao(liquidType) {
+//     console.log("Yeh lo beta glass")
+//     console.log("sambhal k pakarna isko, kiyun k sheeshay ka hai")
+//     console.log("samney jao , " + liquidType + " cooler k pass")
+//     console.log("ek hogi tooti, isko kholo")
+//     console.log(liquidType + " ko glass main girney do")
+//     console.log("dekhna glass sey baahir "+ liquidType +" na nikley")
+//     console.log("jesay hi glass bharney lagey, to tooti band kardo")
+//     console.log("wapis meray pass ana, sambhal k " + liquidType +" k glass ko le kar aana")
+// }
+
+// betaBharKLao("coffee")
+// console.log("=========================")
+// betaBharKLao("chai")
+// console.log("=========================")
+// betaBharKLao("pepsi")
+// console.log("=========================")
+// betaBharKLao(prompt("Kya piyun yeh bata"))
+
+
+// function juiceDeDo(fruit) {                //parameter
+//     console.log("sab sey pehley " + fruit + " ko dho lo")
+//     console.log("phir " + fruit + " k pieces karlo")
+//     console.log("phir " + fruit + " k pieces ko juicer machine main daal do")
+//     console.log("phir " + fruit + " ka juice ek glass main nikal lo")
+//     console.log("aur phir customer ko serve karo")
+// }
+
+
+// juiceDeDo("apple")              //argument
+// console.log("======================")
+// juiceDeDo("mango")
+// console.log("======================")
+// juiceDeDo("narangi")
+
+// function addition(a, b) {                                               //12
+//     var c = "The addition value of " + a + " and " + b + " is " + (a + b)
+
+
+//     return c
+
+//     //blind
+
+
+//     const winner = "alif"
+
+//     console.log(winner)
+
+
+
+
+
+//     return "Winner is falana"
+
+//     console.log("adasdjadjfdslfkskjfkljfalf")
+// }
+
+// console.log(addition(5,7))
+// document.write(addition(5,7))
+// alert(addition(5,7))
+
+
+
+// function hello() {
+//     return "hello sufiyan"
+// }
+
+// hello()  //"hello sufiyan"
+
+// const wallHeight = 80
+
+// function calculate(val) {
+//     var a = "sufiyan"
+//     console.log(a, "==a")
+//     const calculation = val * 2
+//     var b = "farhan"
+
+//     return 420
+
+//     console.log(b, "===b")
+// }
+
+// const width = calculate(200)    // returned value is 420
+
+//                            //80
+// const height = calculate(wallHeight)   // returend value is 160
+
+// const dimensions = [width, height]
+
+// console.log(dimensions, "==>>dimensions")
+
+var std1
+var std2
+var std3
+
+function nameStudents(a, b, c) {
+    std1 = a
+    std2 = b
+    std3 = c
+
+    console.log(std1, std2, std3)
 }
 
-betaBharKLao("coffee")
-console.log("=========================")
-betaBharKLao("chai")
-console.log("=========================")
-betaBharKLao("pepsi")
-console.log("=========================")
-betaBharKLao(prompt("Kya piyun yeh bata"))
+nameStudents("saqib", "fahad", "zain")
