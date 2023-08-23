95d21cf2aedc068a9c1482740d359af125e76a0e
InnoSufiyan <innosufiyan@gmail.com>
2023-08-23 10:00:11 -0700

    if n else

---
+++ JavaScript/Basics/code/ifNelse.js
@@ -0,0 +1,65 @@
+// var myTeacher = prompt("Who is your teacher")
+//           //truth
+// if(myTeacher.toLowerCase() == "sufiyan") {
+//     document.write("Jee haan aap ka teacher " + myTeacher + " hi hai")
+//     // document.write(1)
+// } else {
+//     document.write("Nahin ap ka teacher sufiyan tha , " + myTeacher + " nahin hai")
+//     // document.write(2)
+// }
+
+
+
+var bonus = prompt("How much bonus did you get")
+//"10000"
+if (+bonus >= 50000) {
+    document.write("Cell phone khareedunga")
+} else if(+bonus >= 15000) {
+    document.write("family ko khana khilaaney le jaounga + icecream bhi khilaounga")
+} else if (+bonus >= 10000) {
+    document.write("family ko khana khilaaney le jaounga")
+} else {
+    document.write("ghamgheen gaaney suneyngey")
+}
+
+// var ticketNumber = 32414
+
+// if(ticketNumber === 12333) {
+//     // console.log("jeet gaya hai")
+// } else {
+//     console.log("haar gaya hai, better luck next time")
+// }
+
+// if(ticketNumber !== 12333) {
+//     console.log("haar gaya hai, better luck next time")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
