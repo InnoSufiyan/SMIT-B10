8f332754868ed00c9d4800fea8ae8e3a16f85d02
InnoSufiyan <innosufiyan@gmail.com>
2023-09-17 21:32:58 +0500

    local and global scope

---
+++ JavaScript/Basics/code/globalAndLocalScope.js
@@ -0,0 +1,18 @@
+let name2 = "farhan"
+
+console.log(name2, "==>> name2")
+
+
+{
+    let name = "sufiyan"
+    
+    console.log(name, "==>> name")
+    console.log(name2, "==>> name2")
+
+    var name3 = "salman"
+}
+
+
+let name = "kamran"
+console.log(name, "==>> baahir wala")
+console.log(name3, "==>> baahir wala with var")
