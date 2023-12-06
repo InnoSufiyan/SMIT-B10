7f710b39cfdf7ff1b931646198c70e4c078b7d78
innosufiyan@gmail.com <mohammadsaadali786.92@gmail.com>
2023-12-06 21:06:59 +0500

    delete functionality

--- JavaScript/Basics/code/firstProject/home/app.js
+++ JavaScript/Basics/code/firstProject/home/app.js
@@ -19,23 +19,25 @@ const postsLocalStorage = JSON.parse(localStorage.getItem('posts')) || []
 const postDisplayHandler = () => {
     postContentArea.innerHTML = ""
 
+    const postsLocalStorage = JSON.parse(localStorage.getItem('posts')) || []
+
     postsLocalStorage.reverse().forEach(element => {
         const textHTML = `
-        <div class="card text-center">
-        <div class="card-header" id="userName">
-            ${element.userDetail.userName}
+            <div class="card text-center">
+            <div class="card-header" id="userName">
+               ${loggedInUser.email === element.userDetail.email ? `<button onclick="editHandler(${element.id})">Edit</button> ${element.userDetail.userName} <button onclick="deleteHandler(${element.id})">Delete</button>` : `${element.userDetail.userName}`} 
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special Post</h5>
+                <p class="card-text">${element.textData}.</p>
+            </div>
+            <div class="card-footer text-body-secondary">
+                2 days ago
+            </div>
         </div>
-        <div class="card-body">
-            <h5 class="card-title">Special Post</h5>
-            <p class="card-text">${element.textData}.</p>
-        </div>
-        <div class="card-footer text-body-secondary">
-            2 days ago
-        </div>
-    </div>
-        `
+            `
 
-    postContentArea.innerHTML += textHTML
+        postContentArea.innerHTML += textHTML
     });
 }
 
@@ -53,12 +55,14 @@ const postSubmitHandler = () => {
     if (imageUrl) {
         console.log(imageUrl, "====>>imageUrl")
         postObj = {
+            id: Date.now(),
             textData: postInput.value,
             imgData: imageUrl,
             userDetail: JSON.parse(localStorage.getItem('loggedInUser'))
         }
     } else {
         postObj = {
+            id: Date.now(),
             textData: postInput.value,
             userDetail: JSON.parse(localStorage.getItem('loggedInUser'))
         }
@@ -79,4 +83,16 @@ const logoutHandler = () => {
     localStorage.removeItem('loggedInUser')
 
     window.location.href = '../login/index.html'
+}
+
+const editHandler = () => {
+    console.log("edit handler working properly")
+}
+const deleteHandler = (postId) => {
+    console.log("delete handler working properly", postId)
+    const forDelete = JSON.parse(localStorage.getItem('posts'))
+    const filteredData = forDelete.filter((post) => post.id != postId)
+    console.log(filteredData, "===>> filteredData")
+    localStorage.setItem('posts', JSON.stringify(filteredData))
+    postDisplayHandler()
 }
